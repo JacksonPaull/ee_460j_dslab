aae149fc1e9283418d544c1f0e3230aad705e7b2
Jackson Paull <jackson@planetpaull.com>
2022-11-27 20:18:23 -0600

    Adjusted pong to be single player ready +  config

--- 06_lab_final_project/pong.py
+++ 06_lab_final_project/pong.py
@@ -2,6 +2,9 @@ import pygame
 import random
 import math
 
+import neat
+import pickle
+
 # Defining Global Constants
 c_white = (255, 255, 255)
 c_black = (0  ,   0,   0)
@@ -15,26 +18,44 @@ class GameObject:
 
 class Ball(GameObject):
     r = 10
-    v = 0.5
+    start_vel = 0.5
 
-    def __init__(self, x, y, engine):
+    def __init__(self, x, y, paddle):
         self.x = x
         self.y = y
-        self.engine = engine
+
+        self.start_x = x
+        self.start_y = y
+
+        self.paddle = paddle
 
         dx = (-1 if random.random() > 0.5 else 1) * 0.5 
         dy = (random.random() - 0.5) * 2 
-        mag = math.sqrt(dx ** 2 + dy ** 2)
+        v = math.sqrt(dx ** 2 + dy ** 2)
 
-        self.speed = (dx/mag * self.v, dy/mag * self.v)
+        self.vel = (dx/v * self.start_vel, dy/v * self.start_vel)
+
+    def reset(self):
+        self.x = self.start_x
+        self.y = self.start_y
+
+        dx = (-1 if random.random() > 0.5 else 1) * 0.5 
+        dy = (random.random() - 0.5) * 2 
+        v = math.sqrt(dx ** 2 + dy ** 2)
+
+        self.vel = (dx/v * self.start_vel, dy/v * self.start_vel)
 
     def update(self, dt):
         # Move
-        self.y += self.speed[1] * dt
-        self.x += self.speed[0] * dt
+        self.y += self.vel[1] * dt
+        self.x += self.vel[0] * dt
 
-        angle = math.atan(self.speed[1]/ self.speed[0])
-        if self.speed[0] < 0:
+        #Add a logarithmic speed boost to the ball
+        v = self.vel[0] ** 2 + self.vel[1] ** 2
+        v = math.sqrt(v)
+
+        angle = math.atan(self.vel[1]/ self.vel[0])
+        if self.vel[0] < 0:
             angle += math.pi
 
 
@@ -42,56 +63,45 @@ class Ball(GameObject):
         if self.y < 0:
             self.y *= -1 # Flip distance to top borders
             angle *= -1
-        if self.y > self.engine.scr_height:
-            self.y -= 2*(self.y - self.engine.scr_height)
+        if self.y > self.paddle.engine.scr_height:
+            self.y -= 2*(self.y - self.paddle.engine.scr_height)
             angle *= -1
 
         # Check for collisions with paddles
-        for i, p in enumerate([self.engine.objects['paddle1'], self.engine.objects['paddle2']]):
-            if self.x >= p.x and self.x <= p.x + p.width and self.y >= p.y and self.y <= p.y + p.height:
-                # Ball collided with paddle
-                self.v = math.log(math.exp(self.v)+0.1) #Add a logarithmic speed boost to the ball
-                ydelt = self.y - (p.y + p.height/2)
-                angle = ydelt/p.height * math.pi / 2 #Value ranging from -pi/4 to pi/4 depending upon where the ball hit the paddle
-                if self.speed[0] > 0:
-                    angle = math.pi - angle
+        
+        if self.x >= self.paddle.x and self.x <= self.paddle.x + self.paddle.width and self.y >= self.paddle.y and self.y <= self.paddle.y + self.paddle.height:
+            # Ball collided with paddle
+            self.v = math.log(math.exp(v)+1) #Add a logarithmic speed boost to the ball
+            ydelt = self.y - (self.paddle.y + self.paddle.height/2)
+            angle = ydelt/self.paddle.height * math.pi / 2 #Value ranging from -pi/4 to pi/4 depending upon where the ball hit the paddle
+            if self.vel[0] > 0:
+                angle = math.pi - angle
 
-                if i == 0:
-                    self.engine.objects['scoreboard'].p1Score += 1
-                    self.x = p.x + p.width + self.r
-                else:
-                    self.engine.objects['scoreboard'].p2Score += 1
-                    self.x = p.x - self.r
-                
 
         # Check for round over (Paddle missed a ball)
-        if self.x < 0 or self.x > self.engine.scr_width:
-            self.engine.reset()
+        if self.x < 0:
+            self.x *= -1
+            angle  = math.pi - angle # flip the direction the ball is traveling over the y axis
 
-        # Update Speed
-        mag = self.v # Can update this to be an instance variable to allow for speed up throughout the game
-        self.speed = (mag * math.cos(angle), mag * math.sin(angle))
+        
+        elif self.x > self.paddle.engine.scr_width:
+            # Paddle missed the ball #TODO return false
+            return False
+
+        self.vel = (v * math.cos(angle), v * math.sin(angle))
+        return True
 
     def draw(self, surf):
         pygame.draw.circle(surf, c_white, (self.x, self.y), self.r)
 
-        # Debug code to shoe dx and dy for the ball
-        """t = self.engine.fnt_debug.render(f'Ball Speed: ({self.speed[0]:.2f}, {self.speed[1]:.2f})', False, c_white)
-        t_rect = t.get_rect()
-        t_rect.topleft = (10, 10)
-        surf.blit(t, t_rect)
 
-        t = self.engine.fnt_debug.render(f'Magnitude: {math.sqrt(self.speed[0] ** 2 + self.speed[1] ** 2):.2f}', False, c_white)
-        t_rect = t.get_rect()
-        t_rect.topleft = (10, 60)
-        surf.blit(t, t_rect)"""
-        
 
 
 class Paddle(GameObject):
     width = 25
     height = 135 # Center at y + 67
     speed = 0.5
+    player_controlled=True
     keybinds = {
         'up':pygame.K_w,
         'down':pygame.K_s
@@ -100,15 +110,30 @@ class Paddle(GameObject):
     def __init__(self, x, y, engine, keybinds=None):
         self.x = x
         self.y = y
+        self.start_x = x
+        self.start_y = y
         self.engine = engine
         if keybinds is not None:
             self.keybinds = keybinds
 
+        self.ball = Ball(self.engine.scr_width/2, self.engine.scr_height/2, self)
+
+    def reset(self):
+        self.x = self.start_x
+        self.y = self.start_y
+
+        self.ball.reset()
+
     def update(self, dt):
-        if pygame.key.get_pressed()[self.keybinds['up']]:
-            self.move_up(dt)
-        if pygame.key.get_pressed()[self.keybinds['down']]:
-            self.move_down(dt)
+        if self.player_controlled:
+            if pygame.key.get_pressed()[self.keybinds['up']]:
+                self.move_up(dt)
+            if pygame.key.get_pressed()[self.keybinds['down']]:
+                self.move_down(dt)
+
+        if not self.ball.update(dt):
+            # missed ball
+            self.engine.reset()
         
     def move_up(self, dt):
         self.y -= min(self.speed*dt, self.y)
@@ -120,10 +145,11 @@ class Paddle(GameObject):
 
     def draw(self, surf):
         surf.fill(c_white, (self.x, self.y, self.width, self.height))
-        #pygame.draw.line(surf, c_red, (self.x - 5, self.y + 67), (self.x + self.width + 5, self.y + 67), width=3) # Debug code to show the center of the paddle
+        self.ball.draw(surf)
 
 
 class Scoreboard(GameObject):
+    #TODO Refactor this if we want it to display like the maximum current score?
     dashed_line_height = 25
 
     def __init__(self, font, engine):
@@ -154,6 +180,7 @@ class Scoreboard(GameObject):
             pygame.draw.line(surf, c_white, start_pos, end_pos, 3)
 
 
+
 class Pong_Game_Engine:
     pygame.init()
     pygame.font.init()
@@ -161,6 +188,9 @@ class Pong_Game_Engine:
     clock = pygame.time.Clock()
     scr_width, scr_height = 1280, 720
     fnt_debug = pygame.font.SysFont('Arial', 32)
+
+    dashed_line_height = 25
+    num_lines = math.ceil(scr_height / dashed_line_height / 2)
 
     def __init__(self, screensize=None):
         self.objects = {}
@@ -171,12 +201,10 @@ class Pong_Game_Engine:
             self.scr_width, self.scr_height = screensize
 
         self.screen = pygame.display.set_mode((self.scr_width, self.scr_height))
-        self.objects['ball'] = Ball(self.scr_width/2, self.scr_height/2, self)
-        self.objects['scoreboard'] = Scoreboard(self.fnt_debug, self)
 
-        p = Paddle(30, self.scr_height/2, self)
-        p.y -= math.floor(p.height/2)
-        self.objects['paddle1'] = p
+        #p = Paddle(30, self.scr_height/2, self)
+        #p.y -= math.floor(p.height/2)
+        #self.objects['paddle1'] = p
 
         p = Paddle(self.scr_width - 30, self.scr_height/2, self, keybinds={'up':pygame.K_o, 'down':pygame.K_l})
         p.x -= p.width
@@ -187,6 +215,12 @@ class Pong_Game_Engine:
         self.screen.fill(c_black)
         for o in self.objects.values():
             o.draw(self.screen)
+
+        for i in range(self.num_lines):
+            start_pos = (self.scr_width / 2, self.dashed_line_height * 2 * i)
+            end_pos =   (self.scr_width / 2, self.dashed_line_height * 2 * i + self.dashed_line_height)
+            pygame.draw.line(self.screen, c_white, start_pos, end_pos, 3)
+
         pygame.display.flip()
 
     def update(self):
@@ -210,56 +244,101 @@ class Pong_Game_Engine:
 
         for o in self.objects.values():
             o.draw(self.screen)
+        
+        for i in range(self.num_lines):
+            start_pos = (self.scr_width / 2, self.dashed_line_height * 2 * i)
+            end_pos =   (self.scr_width / 2, self.dashed_line_height * 2 * i + self.dashed_line_height)
+            pygame.draw.line(self.screen, c_white, start_pos, end_pos, 3)
 
         pygame.display.flip()
 
     def reset(self):
-        p1 = self.objects['paddle1']
-        p2 = self.objects['paddle2']
-        ball = self.objects['ball']
         self.paused = True
-
-        p1.x = 30
-        p1.y = self.scr_height/2 - math.floor(p1.height/2)
-
-        p2.x = self.scr_width - 30 - p2.width
-        p2.y = self.scr_height/2 - math.floor(p2.height/2)
-
-        dx = (-1 if random.random() > 0.5 else 1) * 0.5 
-        dy = (random.random() - 0.5) * 2
-        mag = math.sqrt(dx ** 2 + dy ** 2)
-        ball.speed = (dx/mag, dy/mag)
-
-        ball.x, ball.y = self.scr_width/2, self.scr_height/2
 
         self.screen.fill(c_black)
 
         for o in self.objects.values():
+            o.reset()
             o.draw(self.screen)
 
         pygame.display.flip()
 
 
+def eval_genomes(genomes, config):
+    paddles = []
+    nets = []
+    genomes = []
+    
+    for genome_id, genome in genomes:
+        genome.fitness = 4.0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+
+    # Create engine
+        
+    # while loop
+        # update all paddles
+            # If a paddle fails to reflect a ball, 
+        # update engine
+
+        #draw all paddles
+
+        
+
+
+
+def run(config_path):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+
+    winner = p.run(eval_genomes, 300)
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    pickle.dump(winner_net, open('./best_net.pkl', 'wb'))
+
+   
+    
+
 
 if __name__ == '__main__':
+
+    #open config
+
+    #call run
+
     pge = Pong_Game_Engine()
     while not pge.should_quit:
         pge.update()
 
 
 
-# Todo:
-    # - Add Title Screen + press space to unpause
-    # - Add pause screen + press space to unpause
+# TODO
+    # Create run() function
+        # Wrapper function for eval_genomes, includes neat setup
 
-    # Add Paddle collision
+    # Create eval_genomes function
+        # parameters: genomes, config
 
-    # Add Score counter
-        # Increment score on paddle collision
+        # Create a paddle for each genome
 
-    # Implement 'game over' on ball collision with vertical walls
-        # space to re-serve
-        # r to restart
-        # escape to quit
+        # Create one ball for each genome
+            # Refactor ball to be a child class of the paddle
 
-        # (Optional) Game ends after first score to reach 5
+        # One iteration of the game,
+            # score increases for every ball reflection
+            # 
+
+    # Ideas:
+        # Ball speed increases logarithmically throughout the game
+            # any other sublinear way?
+            # Linearly
+
+        # After model is trained play it vs all of us, see what scores we can get, how far the ai gets
+
+        # Inputs
+        # ball x
+        # ball y
+        # ball xspeed
+        # ball yspeed
+        # paddle y
